5a6f051f457ba37124107de515ac40e67e6357e3
Michal <scientific.shelter@gmail.com>
2023-05-29 08:18:12 +0200

    initial commit

---
+++ src/main/java/com/michal/pma/ProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.michal.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementApplication.class, args);
+	}
+
+}
