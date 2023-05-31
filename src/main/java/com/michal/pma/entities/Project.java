3cab50b08509ab65eff349b57afd2c02a459b8ca
Michal <scientific.shelter@gmail.com>
2023-05-31 07:54:34 +0200

    Project class updated

--- src/main/java/com/michal/pma/entities/Project.java
+++ src/main/java/com/michal/pma/entities/Project.java
@@ -1,5 +1,56 @@
 package com.michal.pma.entities;
 
-public class Project {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity 
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long projectID;
+	private String name;
+	private String stage;
+	private String description;
+	
+	public Project() {
+		
+	}
+	
+	public Project(String name, String stage, String description) {
+		super();
+		this.name = name;
+		this.stage = stage;
+		this.description = description;
+	}
+	
+	public long getProjectID() {
+		return projectID;
+	}
+	public void setProjectID(long projectID) {
+		this.projectID = projectID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
 }
